9f9863c472abdcbcc651d54b4143bde94c0b743e
benjamin fuentes <bfuentes@fr.ibm.com>
2017-08-16 17:46:52 +0200

    works on android emulator API 23

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import {Device, MediaPlugin} from 'ionic-native';
 import {NavController, AlertController, Platform} from 'ionic-angular';
 
-
 @Component({
   templateUrl: 'home.html'
 })
@@ -14,31 +13,7 @@ export class HomePage {
               public alertCtrl: AlertController,platform: Platform) {
     platform.ready().then(() => {
       console.log('Now you can use plugins');
-
-      //ios this.media = new MediaPlugin('../Library/NoCloud/recording.wav');
-      //this.media = new MediaPlugin('file:///android_asset/www/recording.wav');
-      let src :string;
-
-      // HTML5 Audio
-      if (typeof Device != "undefined") {
-        console.log("Platform : "+Device.platform);
-        // Android needs the search path explicitly specified
-        if (Device.platform == 'Android') {
-          src = '/android_asset/www/recording.aac';
-        } else if (Device.platform == 'iOS') {
-          src = '../Library/NoCloud/recording.wav';
-        } else {
-          this.showAlert("No OS found", null);
-        }
-
-        if(src)this.media = new MediaPlugin(src);
-
-      }else{
-        this.showAlert("No Device found", null);
-      }
-
-
-
+      this.media = new MediaPlugin('cdvfile://localhost/temporary/recording'+ Device.platform == 'iOS' ? '.wav':'.aac');
     });
   }
 
